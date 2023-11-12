e8855605a9c9abd7674879731b74c67debda7b8c
Derek Ashmore <derek.ashmore@dvtconsulting.com>
2023-11-12 09:15:49 -0500

    Test for automatic documentation on PR

--- modules/security_group/security_group.tf
+++ modules/security_group/security_group.tf
@@ -5,7 +5,29 @@ resource "azurerm_network_security_group" "security_group" {
   tags                  = var.tags
 }
 
-output azurerm_network_security_group {
-  value = azurerm_network_security_group.security_group
-  description = "Security group AzureRM object"
+resource "azurerm_network_security_rule" "security_group_rule" {
+  for_each = var.rule_config
+
+  resource_group_name         = azurerm_network_security_group.security_group.resource_group_name
+  network_security_group_name = azurerm_network_security_group.security_group.name
+
+  name                        = each.key
+  priority                    = each.value.priority
+  protocol                    = each.value.protocol
+  direction                   = each.value.direction
+  access                      = each.value.access
+  description                 = each.value.description
+
+  source_port_range                       = each.value.source_port_range
+  source_port_ranges                      = each.value.source_port_ranges
+  source_address_prefix                   = each.value.source_address_prefix
+  source_address_prefixes                 = each.value.source_address_prefixes
+  source_application_security_group_ids   = each.value.source_application_security_group_ids
+
+  destination_port_range                      = each.value.destination_port_range
+  destination_port_ranges                     = each.value.destination_port_ranges
+  destination_address_prefix                  = each.value.destination_address_prefix
+  destination_address_prefixes                = each.value.destination_address_prefixes
+  destination_application_security_group_ids  = each.value.destination_application_security_group_ids
+
 }
---
+++ modules/security_group/security_group_outputs.tf
@@ -0,0 +1,5 @@
+
+output azurerm_network_security_group {
+  value         = azurerm_network_security_group.security_group
+  description   = "Security group AzureRM object"
+}
